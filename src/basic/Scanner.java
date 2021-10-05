9004ebe6c98fcc78e9f97f0f7c94afc5503151e7
Simun20 <simunabyan@gmail.com>
2021-10-04 23:08:55 -0400

    First commit

---
+++ src/basic/Scanner.java
@@ -0,0 +1,20 @@
+package basic;
+
+import java.sql.SQLOutput;
+
+public class Scanner {
+
+    public static void main(String[] args) {
+
+        System.out.println("simun");
+
+        System.out.println(3000);
+
+
+
+    }
+
+
+
+
+}
